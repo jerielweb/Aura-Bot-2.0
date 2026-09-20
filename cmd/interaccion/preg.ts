0687fb5f2b48a2b275ef3ac962ac3b999ac31e50
BostHost <lopezdavison7@gmail.com>
2026-09-20 11:36:59 -0600

    Actualizar preg.ts

--- cmd/interaccion/preg.ts
+++ cmd/interaccion/preg.ts
@@ -53,7 +53,7 @@ export default {
 				caption = `\`${senderName}\` ${fytBold("preñó a")} \`${targetName}\` 🤰👶`;
 				mentions = [sender, targetJid];
 			} else {
-				caption = `\`${senderName}\` ${fytBold("quiere preñar")} 🤰`;
+				caption = `\`${senderName}\` ${fytBold("quiere preñar ah alguien")} 🤰`;
 			}
 
 			await react("✅");
