e42228742ec9f2465ec7ad1cc7bbf10c277d1980
BostHost <lopezdavison7@gmail.com>
2026-09-20 16:01:31 -0600

    Actualizar bite.ts

--- cmd/interaccion/bite.ts
+++ cmd/interaccion/bite.ts
@@ -55,7 +55,7 @@ export default {
 				caption = `\`${senderName}\` ${fytBold("quiere morder")} 😬`;
 			}
 
-			await react("✅");
+			
 			await reply({
 				video: { url: data.result },
 				caption,
