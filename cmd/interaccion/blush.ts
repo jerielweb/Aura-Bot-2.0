25628a77cb286926d96543add2265d377c887121
BostHost <lopezdavison7@gmail.com>
2026-09-20 16:02:21 -0600

    Actualizar blush.ts

--- cmd/interaccion/blush.ts
+++ cmd/interaccion/blush.ts
@@ -55,7 +55,7 @@ export default {
 				caption = `\`${senderName}\` ${fytBold("está sonrojado")} 😳`;
 			}
 
-			await react("✅");
+			
 			await reply({
 				video: { url: data.result },
 				caption,
