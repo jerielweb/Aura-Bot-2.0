681ab426f462aa400bbe9c9af5acaf39e0043f93
BostHost <lopezdavison7@gmail.com>
2026-09-21 10:20:38 -0600

    Actualizar tickle.ts

--- cmd/interaccion/tickle.ts
+++ cmd/interaccion/tickle.ts
@@ -55,7 +55,7 @@ export default {
 				caption = `\`${senderName}\` ${fytBold("quiere hacer cosquillas")} 🤭`;
 			}
 
-			await react("✅");
+			
 			await reply({
 				video: { url: data.result },
 				caption,
