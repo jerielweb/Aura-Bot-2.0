7704356206ec1f2a09b899578c75789ce90d4bbf
BostHost <lopezdavison7@gmail.com>
2026-09-21 10:21:16 -0600

    Actualizar wink.ts

--- cmd/interaccion/wink.ts
+++ cmd/interaccion/wink.ts
@@ -55,7 +55,6 @@ export default {
 				caption = `\`${senderName}\` ${fytBold("quiere guiñar el ojo")} 😉`;
 			}
 
-			await react("✅");
 			await reply({
 				video: { url: data.result },
 				caption,
