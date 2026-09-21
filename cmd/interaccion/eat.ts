ac4a43ece25a54f0a353a41a102fd1ceb2db16d7
BostHost <lopezdavison7@gmail.com>
2026-09-21 10:13:33 -0600

    Actualizar eat.ts

--- cmd/interaccion/eat.ts
+++ cmd/interaccion/eat.ts
@@ -55,7 +55,7 @@ export default {
 				caption = `\`${senderName}\` ${fytBold("quiere comer")} 🍽️`;
 			}
 
-			await react("✅");
+			
 			await reply({
 				video: { url: data.result },
 				caption,
