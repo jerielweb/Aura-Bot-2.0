f7596166f8ad0d3d4035f14259f2fd12e9badd25
BostHost <lopezdavison7@gmail.com>
2026-09-21 10:16:36 -0600

    Actualizar lick.ts

--- cmd/interaccion/lick.ts
+++ cmd/interaccion/lick.ts
@@ -55,7 +55,7 @@ export default {
 				caption = `\`${senderName}\` ${fytBold("quiere lamer")} 👅`;
 			}
 
-			await react("✅");
+			
 			await reply({
 				video: { url: data.result },
 				caption,
