d5cc9ca40489ab8181e3ef00e92207bcba6517a7
BostHost <lopezdavison7@gmail.com>
2026-09-21 10:19:24 -0600

    Actualizar slap.ts

--- cmd/interaccion/slap.ts
+++ cmd/interaccion/slap.ts
@@ -55,7 +55,6 @@ export default {
 				caption = `\`${senderName}\` ${fytBold("quiere dar una bofetada")} 💥`;
 			}
 
-			await react("✅");
 			await reply({
 				video: { url: data.result },
 				caption,
