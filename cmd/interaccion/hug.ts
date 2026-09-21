7701ae58ee996202fd71af2922f9ced0bd1a52d9
BostHost <lopezdavison7@gmail.com>
2026-09-21 10:15:25 -0600

    Actualizar hug.ts

--- cmd/interaccion/hug.ts
+++ cmd/interaccion/hug.ts
@@ -55,7 +55,7 @@ export default {
 				caption = `\`${senderName}\` ${fytBold("quiere un abrazo")} 🤗`;
 			}
 
-			await react("✅");
+		
 			await reply({
 				video: { url: data.result },
 				caption,
